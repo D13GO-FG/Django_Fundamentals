7a070dfa7a6827a012769174e6211f9fe5e4f1ed
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-06 12:43:30 -0700

    Update app to use pandas

---
+++ web-server/store.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    catergories = r.json()
+    for category in catergories:
+        print(category['name'])
