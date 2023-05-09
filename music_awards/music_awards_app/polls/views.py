5b7a2ccf1a78facba90f3054d52021d8e43fe6bb
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-09 16:22:57 -0700

    Add templeates

--- music_awards/music_awards_app/polls/views.py
+++ music_awards/music_awards_app/polls/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("You are in the web page of music awards app")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
-def detail(request, questions_id):
-    return HttpResponse(f"You are seeing the question number {questions_id}")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
-def results(request, questions_id):
-    return HttpResponse(f"You are seeing the results of question number {questions_id}")
+def results(request, question_id):
+    return HttpResponse(f"You are seeing the results of question number {question_id}")
 
-def vote(request, questions_id):
-    return HttpResponse(f"You are voting to the question number {questions_id}")
+def vote(request, question_id):
+    return HttpResponse(f"You are voting to the question number {question_id}")
