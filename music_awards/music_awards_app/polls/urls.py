9809df7f69bcf9d2d7d4021da07f7b30bad91660
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-08 21:13:32 -0700

    Added functions views

--- music_awards/music_awards_app/polls/urls.py
+++ music_awards/music_awards_app/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # ex. /polls/
+    path("", views.index, name="index"),
+    # ex. /polls/5/
+    path("<int:questions_id>/", views.detail, name="index"),
+    # ex. /polls/5/results
+    path("<int:questions_id>/results/", views.results, name="index"),
+    # ex. /polls/5/vote
+    path("<int:questions_id>/vote/", views.vote, name="index"),
 ]
--- music_awards/music_awards_app/polls/views.py
+++ music_awards/music_awards_app/polls/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("You are in the web page of music awards app")
 
+def detail(request, questions_id):
+    return HttpResponse(f"You are seeing the question number {questions_id}")
+
+def results(request, questions_id):
+    return HttpResponse(f"You are seeing the results of question number {questions_id}")
+
+def vote(request, questions_id):
+    return HttpResponse(f"You are voting to the question number {questions_id}")
