48d256f48182dbedbd4a54c8f493f9997380ecbd
Diego_Flores <ldiego.flores98@gmail.com>
2023-05-09 20:50:10 -0700

    Updated vote view

--- music_awards/music_awards_app/polls/views.py
+++ music_awards/music_awards_app/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -19,4 +20,16 @@ def results(request, question_id):
     return HttpResponse(f"You are seeing the results of question number {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"You are voting to the question number {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You don't choose any anwser"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
